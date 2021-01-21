ac353277d83d051ec61460464ef52bc28f8b98d2
adele <adele.r87@gmail.com>
2021-01-21 14:52:08 +0100

    inizio

---
+++ js/main.js
@@ -0,0 +1,146 @@
+// Milestone 1:
+// - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
+// - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
+
+const arrayOggetti = [
+   {
+     'name' : 'cat',
+      'prefisso': 'c',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'crow',
+      'prefisso': 'c',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'dog',
+      'prefisso': 'd',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'dove',
+      'prefisso': 'd',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'dragon',
+      'prefisso': 'd',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'horse',
+      'prefisso': 'h',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'hippo',
+      'prefisso': 'h',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'fish',
+      'prefisso': 'f',
+      'type': 'animal',
+      'family': 'fas',
+   },
+   {
+     'name' : 'carrot',
+      'prefisso': 'c',
+      'type': 'vegetable',
+      'family': 'fas',
+   },
+   {
+     'name' : 'apple-alt',
+      'prefisso': 'a',
+      'type': 'vegetable',
+      'family': 'fas',
+   },
+   {
+     'name' : 'lemon',
+      'prefisso': 'l',
+      'type': 'vegetable',
+      'family': 'fas',
+   },
+   {
+     'name' : 'pepper-hot',
+      'prefisso': 'p',
+      'type': 'vegetable',
+      'family': 'fas',
+   },
+   {
+     'name' : 'user-astronaut',
+      'prefisso': 'u',
+      'type': 'human',
+      'family': 'fas',
+   },
+   {
+     'name' : 'user-graduat',
+      'prefisso': 'u',
+      'type': 'human',
+      'family': 'fas',
+   },
+   {
+     'name' : 'user-ninja',
+      'prefisso': 'u',
+      'type': 'human',
+      'family': 'fas',
+   },
+ ]
+
+// popolo di icone i div che mi sono creata su html
+
+arrayOggetti.forEach((element) => {
+
+});
+
+
+
+
+// Gatto
+const iconCat = document.getElementById('icon-cat');
+
+iconCat.innerHTML+=
+`
+<div>
+  <i class="fas fa-cat"></i>
+</div>
+
+`
+// crow
+const iconCrow = document.getElementById('icon-crow');
+
+iconCrow.innerHTML+=
+`
+<div>
+<i class="fas fa-crow"></i>
+</div>
+
+`
+// dog
+const iconDog = document.getElementById('icon-dog');
+
+iconDog.innerHTML+=
+`
+<div>
+<i class="fas fa-dog"></i>
+</div>
+
+`
+// dog
+const iconDog = document.getElementById('icon-dog');
+
+iconDove.innerHTML+=
+`
+<div>
+<i class="fas fa-dove"></i>
+</div>
+
+`
