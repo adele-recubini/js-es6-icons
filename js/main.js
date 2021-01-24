5a17e3cbdf0d6a8de247a69194c10e729f2325fb
adele <adele.r87@gmail.com>
2021-01-24 16:57:34 +0100

    js-es6-icons

--- js/main.js
+++ js/main.js
@@ -148,38 +148,64 @@ if(element.type === 'animal') {
 });
 console.log(arrayOggetti);
 
+// ora voglio poter scegliere cosa vedere nel html in base ai valori che seleziono nel value (che ora è vuoto)
 
+ // prendo il mio arraydi oggetti e li filtro inbase alla chiave TYPE che avra valor human animals e vegetable
 
+ const allType = []
+ const selector =document.getElementById('select')
 
-const colorSelect = document.getElementsByClassName('color-select')[0];
+ arrayOggetti.forEach((element) => {
 
-// filtriamo in base al colore creando un array vuoto dove andranno tutti i miei oggetti associati alla chiave colore
-const colorList = [];
-arrayOggetti.forEach((element) => {
-  if(!colorList.includes(element.color)) {
-    colorList.push(element.color);
-  }
-});
+   if (!allType.includes(element.type)) {
+     allType.push(element.type)
+   }
 
-// per tutti i colori associo gli elementi che andranno a popolare nell html la classe color select e come valori gli elementi
-colorList.forEach((element) => {
-  colorSelect.innerHTML += `
-  <option value="${element}">${element}</option>
+ });
+console.log(allType);
+// ora questy type cioe human , vegetable e animal devo inserirli letteralmente nei valori dell otion creato nell html
+// come li inserisco ?  e dove le inserisco nel selector con id nel html creando una costante e andranno a popolare le value della sua option. Select e option in html vanno insieme con un inner innerHTML
+
+allType.forEach((element) => {
+
+  selector.innerHTML+=`
+
+    <option value="${element}">${element}</option>
+
   `
 });
+// ora devo creare un addvent listener cioe l utente clicca un type human vegetable e animals e succederà qualcosa nel mio caso mi farà vedere solo le icone con quel type
 
-const selector = $('.color-select');
+// variabilizzo ancora il selector però come elemento
+const typeSelector = $('#select');
 
-selector.change(function() {
-  let valore = $(this).val();
+typeSelector.change(function(){
 
-  if (valore === 'blue' ) {
-     arrayOggetti.filter((element) => {
-    return element.color === 'blue'
-  
-  })
+  // all inizio il container mi dovra far vedere tutte le card lo faccio sempre con l innerHTML
 
-  }
+  myContainer.innerHTML = ''
 
+  // variabilizzo anche l altra funzione cioe quando clicco proprio una opzione se la tipologia di selezione è diversa da all (che me le farebbe vedere tutte )  allora filtro il mio array di oggetti per typo humano vegetale o animale per filtrare qualcosa mi serve una costante dove metto la roba filtrata
 
-})
+    const typeSelected = $(this).val()
+    let filtered ;
+
+    if (typeSelected !== 'all') {
+
+      filtered = arrayOggetti.filter((element) =>{
+        return element.type === typeSelected
+      })
+    }else {
+      filtered = arrayOggetti;
+    }
+
+    filtered.forEach((element) => {
+      myContainer.innerHTML += `
+      <div class = "icone">
+        <i class="${element.family} ${element.prefisso}-${element.name}"></i>
+      </div>
+
+      `
+    });
+
+});
